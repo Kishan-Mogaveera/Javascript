dde20a9f3018c7378bce0c1c9e6ab002977ab54e
Kishan-Mogaveera <kishanmogaveera7@gmail.com>
2026-08-12 16:58:13 +0530

    datatype summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive datatype
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); //false
+
+const bigNumber = 3244764946887952n
+console.log(typeof bigNumber);
+
+
+
+// Reference type (Non Primitive)
+// Array, Objects, Functions
+
+const heros = ["shakthiman", "naagraj", "doga"]
+let myObject = {
+    name: "Kishan",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
