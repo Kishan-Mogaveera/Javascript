82a7363556ee8e3730d647074584eda104fd3f33
Kishan-Mogaveera <kishanmogaveera7@gmail.com>
2026-08-21 14:31:32 +0530

    array

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -2,21 +2,21 @@ const marvel_heros = ["IronMan","Spidy", "Thor"]
 const dc_heros = ["superman", "flash", "batman"]
 
 //pushing second array in the first array
-// marvel_heros.push(dc_heros)
-// console.log(marvel_heros);          //[ 'IronMan', 'Spidy', 'Thor', [ 'superman', 'flash', 'batman' ] ]
-// console.log(marvel_heros[3]);       //[ 'superman', 'flash', 'batman' ]
-// console.log(marvel_heros[3][1]);    //flash
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);          //[ 'IronMan', 'Spidy', 'Thor', [ 'superman', 'flash', 'batman' ] ]
+console.log(marvel_heros[3]);       //[ 'superman', 'flash', 'batman' ]
+console.log(marvel_heros[3][1]);    //flash
 
 //concatinatng 2 arrays
-// const allheros = marvel_heros.concat(dc_heros)
-// console.log(allheros);              //[ 'IronMan', 'Spidy', 'Thor', 'superman', 'flash', 'batman' ]
+const allheros = marvel_heros.concat(dc_heros)
+console.log(allheros);              //[ 'IronMan', 'Spidy', 'Thor', 'superman', 'flash', 'batman' ]
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]
-// console.log(all_new_heros);         //[ 'IronMan', 'Spidy', 'Thor', 'superman', 'flash', 'batman' ]
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);         //[ 'IronMan', 'Spidy', 'Thor', 'superman', 'flash', 'batman' ]
 
-// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-// const real_another_array = another_array.flat(Infinity)
-// console.log(real_another_array);         //[1, 2, 3, 4, 5,6, 7, 6, 7, 4,5]
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);         //[1, 2, 3, 4, 5,6, 7, 6, 7, 4,5]
 
 console.log(Array.isArray("Kishan"));       //false
 console.log(Array.from("Kishan"));          //[ 'K', 'i', 's', 'h', 'a', 'n' ]
